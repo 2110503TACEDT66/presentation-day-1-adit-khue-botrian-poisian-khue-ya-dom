ff47f3cfdb1f6ea606725ccf918a2a5983b46aaa
Unique <supitchaya3766@gmail.com>
2024-03-02 23:39:24 +0700

    Register User / Login

--- routes/restaurants.js
+++ routes/restaurants.js
@@ -3,7 +3,9 @@ const {getRestaurants,getRestaurant,createRestaurant,updateRestaurant,deleteRest
 
 const router = express.Router();
 
-router.route('/').get(getRestaurants).post(createRestaurant);
-router.route('/:id').get(getRestaurant).put(updateRestaurant).delete(deleteRestaurant);
+const {protect , authorize} = require('../middleware/auth');
+
+router.route('/').get(getRestaurants).post(protect, authorize('admin'),createRestaurant);
+router.route('/:id').get(getRestaurant).put(protect ,authorize('admin'),updateRestaurant).delete(protect, authorize('admin'),deleteRestaurant);
 
 module.exports = router;
--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const dotenv = require("dotenv");
+const cookieparser = require('cookie-parser');
 //Load env vars
 const connectDB = require('./config/db') ;
 
@@ -12,9 +13,11 @@ connectDB() ;
 const app = express();
 
 app.use(express.json());
-
+app.use(cookieparser());
 const restaurants = require('./routes/restaurants');
+const auth = require('./routes/auth');
 app.use('/api/v1/restaurants', restaurants);
+app.use('/api/v1/auth', auth);
 
 const PORT = process.env.PORT || 3000;
 const server = app.listen(PORT, console.log(`Server running in`, process.env.NODE_ENV, ` mode on port `, PORT));
