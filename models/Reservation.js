0bb93b283f484640ea76f091f44aaadf60b614ba
Bpan tan <bpantan@Bonk.local>
2024-03-03 22:41:43 +0700

    reser change models

--- models/Reservation.js
+++ models/Reservation.js
@@ -1,24 +1,23 @@
 
 const mongoose = require('mongoose');
 const ReservationSchema=new mongoose.Schema({
-apptDate: {
-type: Date,
-required: true
-},
 user: {
     type:mongoose.Schema.ObjectId,
 ref: 'User',
 required: true
 },
-amount:{
-    type:Number,
-    required:true
-},
-
 restaurant:{
 type: mongoose.Schema.ObjectId,
 ref: 'Restaurant',
 required: true
+},
+reserDate: {
+    type: Date,
+    required: true
+},
+amount:{
+    type:Number,
+    required:true
 },
 createdAt: {
 type: Date,
